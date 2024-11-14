0834e2b1cae5bb2ed8ad174371e7a495b668ad63
LinuxA <aljaz.resnik@gmail.com>
2024-11-14 10:23:32 +0100

    add new invoice logic and improve pdf file naming

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,47 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { format } from "date-fns";
 import "./App.css";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
-import { Button } from "./components/Button/Button.jsx";
 import { Form } from "./components/Form/Form.jsx";
 import { InvoicePage } from "./components/InvoicePage/InvoicePage.jsx";
 
 export const App = () => {
+  const currentDate = new Date();
+
+  const invoiceNumber = `#${format(currentDate, "yy")}-${format(
+    currentDate,
+    "DDD"
+  ).padStart(3, "0")}`;
   const [formData, setFormData] = useState(null);
-  const [isFormComplete, setFormComplete] = useState(false);
 
   const printDocument = () => {
     const input = document.getElementById("invoice-container");
-    html2canvas(input, { scale: 2 }) // Increasing scale for better resolution
-      .then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-        const pdf = new jsPDF({
-          orientation: "portrait",
-          unit: "mm",
-          format: "a4",
-        });
-
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-        pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
-        pdf.save("invoice.pdf");
+    html2canvas(input, { scale: 2 }).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "mm",
+        format: "a4",
       });
+
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save(`${invoiceNumber}-invoice.pdf`);
+    });
   };
 
   const handleFormComplete = (data) => {
     setFormData(data);
-    setFormComplete(true);
+  };
+
+  const handleNewInvoice = () => {
+    setFormData(null); // Reset form data
   };
 
   return (
@@ -48,16 +55,11 @@ export const App = () => {
           path="/"
           element={<Navigate to="/form" />}
         />
-
         <Route
           path="/form"
           element={
             <div id="invoice-container">
-              {!isFormComplete ? (
-                <Form onComplete={handleFormComplete} />
-              ) : (
-                <Button onClick={printDocument}>Convert to PDF</Button>
-              )}
+              <Form onComplete={handleFormComplete} />
             </div>
           }
         />
@@ -65,15 +67,11 @@ export const App = () => {
           path="/invoice"
           element={
             <div id="invoice-container">
-              <InvoicePage formData={formData} />
-              <div className="button-container">
-                <Button
-                  type="button"
-                  onClick={printDocument}
-                >
-                  Convert to PDF
-                </Button>
-              </div>
+              <InvoicePage
+                formData={formData}
+                printDocument={printDocument}
+                onNewInvoice={handleNewInvoice}
+              />
             </div>
           }
         />
--- src/components/InvoicePage/InvoicePage.jsx
+++ src/components/InvoicePage/InvoicePage.jsx
@@ -1,28 +1,43 @@
-import "./InvoicePage.css";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button/Button";
 import { Header } from "../Header/Header";
 import { CustomerAddress } from "../CustomerAddress/CustomerAddress";
 import { TableInformation } from "../TableInformation/TableInformation";
 import { BankDetails } from "../BankDetails/BankDetails";
 import { Footer } from "../Footer/Footer";
+import "./InvoicePage.css";
 
-export const InvoicePage = ({ formData }) => {
-  const { services } = formData;
+export const InvoicePage = ({ formData, printDocument, onNewInvoice }) => {
+  const navigate = useNavigate();
+
+  const handleNewInvoiceClick = () => {
+    onNewInvoice();
+    navigate("/form");
+  };
 
   return (
-    <div className="invoice-page">
-      <Header {...formData} />
-
-      <br />
-      <CustomerAddress {...formData} />
-
-      <br />
-      <TableInformation services={services} />
-
-      <br />
-      <BankDetails {...formData} />
-
-      <br />
-      <Footer />
-    </div>
+    <>
+      <div className="invoice-page">
+        <Header {...formData} />
+        <CustomerAddress {...formData} />
+        <TableInformation services={formData.services} />
+        <BankDetails {...formData} />
+        <Footer />
+      </div>
+      <div>
+        <Button
+          type="button"
+          onClick={handleNewInvoiceClick}
+        >
+          New Invoice
+        </Button>
+        <Button
+          type="button"
+          onClick={printDocument}
+        >
+          Convert to PDF
+        </Button>
+      </div>
+    </>
   );
 };
